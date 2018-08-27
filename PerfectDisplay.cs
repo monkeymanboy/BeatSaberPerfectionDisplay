d58a2acfc134b9dc635a26e19d2ea1d2ff6d3992
dcljr <dcljr@me.com>
2018-08-26 21:53:52 -0700

    first commit

---
+++ PerfectDisplay.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+namespace PerfectionDisplay
+{
+    class PerfectDisplay : MonoBehaviour
+    {
+        TextMeshPro scoreMesh;
+        ScoreController scoreController;
+
+        int[] scoreRanges = { 100, 90, 50 };
+        string[] colors = { "blue", "green", "yellow", "orange", "red" };
+        int[] scoreCount;
+        int misses;
+        IEnumerator WaitForLoad()
+        {
+            bool loaded = false;
+            while (!loaded)
+            {
+                scoreController = Resources.FindObjectsOfTypeAll<ScoreController>().FirstOrDefault();
+                if (scoreController == null)
+                    yield return new WaitForSeconds(0.1f);
+                else
+                    loaded = true;
+            }
+
+            Init();
+        }
+        void Awake()
+        {
+            scoreCount = new int[scoreRanges.Length+1];
+            StartCoroutine(WaitForLoad());
+        }
+        private void Init()
+        {
+            scoreMesh = this.gameObject.AddComponent<TextMeshPro>();
+            scoreMesh.text = "";
+            scoreMesh.fontSize = 3;
+            scoreMesh.color = Color.white;
+            scoreMesh.font = Resources.Load<TMP_FontAsset>("Teko-Medium SDF No Glow");
+            scoreMesh.alignment = TextAlignmentOptions.Center;
+            scoreMesh.rectTransform.position = Plugin.scoreCounterPosition;
+            if (scoreController != null)
+            {
+                scoreController.noteWasMissedEvent += Miss;
+                scoreController.noteWasCutEvent += Cut;
+            }
+            UpdateText();
+        }
+        public void Miss(NoteData data, int c)
+        {
+            misses++;
+            UpdateText();
+        }
+        public void Cut(NoteData data, NoteCutInfo info, int combo)
+        {
+            bool didDone = false;
+            info.afterCutSwingRatingCounter.didFinishEvent += c => {
+                if (didDone) return;
+                didDone = true;
+                ScoreController.ScoreWithoutMultiplier(info, info.afterCutSwingRatingCounter, out int before, out int after);
+                int total = before + after;
+                for (int i = 0; i < scoreRanges.Length; i++)
+                {
+                    if (scoreRanges[i] <total)
+                    {
+                        scoreCount[i]++;
+                        UpdateText();
+                        return;
+                    }
+                }
+                scoreCount[scoreRanges.Length]++;
+                UpdateText();
+            };
+        }
+
+        public void UpdateText()
+        {
+            String text = "";
+            for(int i = 0; i < scoreRanges.Length; i++)
+            {
+                text += "<color=\""+colors[i] +"\">"+">"+scoreRanges[i] + "-" + scoreCount[i] + "<color=\"black\">|";
+            }
+            text += "<color=\"" + colors[scoreRanges.Length] + "\">" + "<" + scoreRanges[scoreRanges.Length-1] + "-" + scoreCount[scoreRanges.Length] + "<color=\"black\">|";
+            text += "<color=\"" + colors[scoreRanges.Length + 1] + "\">" + "MISS-" +misses;
+            scoreMesh.text = text;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,60 @@
+﻿using IllusionPlugin;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PerfectionDisplay
+{
+    public class Plugin : IPlugin
+    {
+        public string Name => "Perfection Display";
+        public string Version => "1.0";
+
+        private readonly string[] env = { "DefaultEnvironment", "BigMirrorEnvironment", "TriangleEnvironment", "NiceEnvironment" };
+        bool _init = false;
+
+        public static Vector3 scoreCounterPosition = new Vector3(0, 2.3f, 7f);
+
+        public void OnApplicationStart()
+        {
+            SceneManager.activeSceneChanged += OnSceneChanged;
+        }
+
+        private void SceneManagerOnActiveSceneChanged(Scene arg0, Scene arg1)
+        {
+        }
+
+        private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
+        }
+
+        public void OnApplicationQuit()
+        {
+            SceneManager.activeSceneChanged -= OnSceneChanged;
+        }
+
+        public void OnLevelWasLoaded(int level)
+        {
+
+        }
+
+        public void OnLevelWasInitialized(int level)
+        {
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+        public void OnFixedUpdate()
+        {
+        }
+
+        private void OnSceneChanged(Scene _, Scene scene)
+        {
+            if (!env.Contains(scene.name)) return;
+
+            new GameObject("PerfectDisplay").AddComponent<PerfectDisplay>();
+        }
+    }
+}
